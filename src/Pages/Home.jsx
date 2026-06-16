6f1a77c497c464db428e548879e1c5f814c93e57
Dhruv Kumar <tumhara-github-email@gmail.com>
2026-06-16 20:49:27 +0530

    Fix Hero import path

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar/Navbar.jsx";
 import SecondaryNavbar from "../components/SecondaryNavbar/SecondaryNavbar.jsx";
-import Hero from "../components/Hero/Hero.jsx";
+import Hero from "../components/hero/Hero.jsx";
 import ProductSection from "../components/ProductSection/ProductSection.jsx";
 import CategorySection from "../components/CategorySection/CategorySection.jsx";
 import Footer from "../components/Footer/Footer.jsx";
