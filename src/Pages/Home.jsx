2a94f44f50260077ecb69b6067209674a9dd19f0
Dhruv Kumar <tumhara-github-email@gmail.com>
2026-06-17 16:35:18 +0530

    Mobile UI Improved

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,22 @@
 import Navbar from "../components/Navbar/Navbar.jsx";
 import SecondaryNavbar from "../components/SecondaryNavbar/SecondaryNavbar.jsx";
 import Hero from "../components/hero/Hero.jsx";
-import ProductSection from "../components/ProductSection/ProductSection.jsx";
 import CategorySection from "../components/CategorySection/CategorySection.jsx";
+import ProductSection from "../components/ProductSection/ProductSection.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
 function Home() {
   return (
     <>
       <Navbar />
-       <SecondaryNavbar />
-      <Hero/>
-         <ProductSection />
-         <CategorySection />
-          <Footer />
-       
+      <SecondaryNavbar />
+      <Hero />
+
+      <CategorySection />
+
+      <ProductSection />
+
+      <Footer />
     </>
   );
 }
--- src/components/CategorySection/CategorySection.jsx
+++ src/components/CategorySection/CategorySection.jsx
@@ -5,19 +5,23 @@ function CategorySection() {
     <div className="categories">
 
       <div className="category-box">
-        Electronics
+        <h3>Electronics</h3>
+        <p>Mobiles, Laptops & Accessories</p>
       </div>
 
       <div className="category-box">
-        Fashion
+        <h3>Fashion</h3>
+        <p>Clothing, Shoes & Watches</p>
       </div>
 
       <div className="category-box">
-        Home
+        <h3>Home</h3>
+        <p>Furniture & Home Essentials</p>
       </div>
 
       <div className="category-box">
-        Gaming
+        <h3>Gaming</h3>
+        <p>Consoles & Gaming Accessories</p>
       </div>
 
     </div>
